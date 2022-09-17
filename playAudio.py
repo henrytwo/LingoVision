d5781390389c69930795323909732b0360d0af20
Shengbuo Fang <36614672+CodingChicken202526@users.noreply.github.com>
2022-09-17 19:02:08 -0400

    Add files via upload

---
+++ playAudio.py
@@ -0,0 +1,12 @@
+from playsound import playsound
+
+def startSound():
+    playsound("StartSound.mp3")
+
+def errorSound():
+    playsound("ErrorSound.mp3")
+
+startSound()
+
+print(7)
+errorSound()
