5e48591ac6af3576771165017a4e4c977ded07d6
Shengbuo Fang <36614672+CodingChicken202526@users.noreply.github.com>
2022-09-18 00:53:58 -0400

    Add files via upload

--- LingoVisionTranslator.py
+++ LingoVisionTranslator.py
@@ -9,7 +9,6 @@ def translateText(untranslated, userLanguage):
         key = f.readlines()
 
     key = key[0]
-    #print(key)
     translated = ""
 
     auth_key = key  # Replace with your key
@@ -19,8 +18,28 @@ def translateText(untranslated, userLanguage):
     result = translator.translate_text(untranslated, target_lang=userLanguage)
 
     translated = result.text
-
+    
     return translated
 
-if __name__ == '__main__':
-    print(translateText("bonjour, jemappelle john", "EN-US"))
+
+print(translateText("bonjour, jemappelle john", "EN-US"))
+
+def getSourceLanguage(untranslated, userLanguage):
+    
+    with open('TranslatorKey.txt') as f:
+        key = f.readlines()
+
+    key = key[0]
+    translated = ""
+
+    auth_key = key  # Replace with your key
+    translator = deepl.Translator(auth_key)
+
+    
+    result = translator.translate_text(untranslated, target_lang=userLanguage)
+
+    sourceLang = result.detected_source_lang
+
+    return sourceLang
+
+print(getSourceLanguage("bonjour, jemappelle john", "EN-US"))
